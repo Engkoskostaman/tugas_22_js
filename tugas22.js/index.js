2c1cbc129830e07efb6ef5221079559b7ad16fa1
Engkos <engkoskostaman97@gmail.com>
2019-08-13 09:18:11 +0700

    tugas 22 js
    tugas ini kami kumpulkan untuk niomic

---
+++ tugas22.js/index.js
@@ -0,0 +1,9 @@
+function panggilforeach(){
+  var data =["saya","ingin","belajar","bersama"];
+  data.forEach (function (item,index){
+    console.log("item :",item,"index ke ",index);
+
+
+  })
+}
+panggilforeach()
